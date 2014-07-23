7b822fffa69af05f79413d927a446affb146cb28
artwebs <artwebs@126.com>
2014-07-23 10:51:01 +0800

    解决加密模块加密模块

--- awspy/security/DESUtils.py
+++ awspy/security/DESUtils.py
@@ -1,30 +1,46 @@
+# -*- coding:utf-8 -*-
 __author__ = 'artwebs'
 
 from Crypto.Cipher import DES3
 from Crypto import Random
-from struct import *
 import base64
+from awspy.common.Method import *
 from awspy.security.Security import *
 
+
 class DESUtils(Security):
-    def encode(self,key,source):
+    def encode(self,source,key,iv):
         key=self.getKey(key)
-        iv = Random.new().read(DES3.block_size)
-        cipher = DES3.new(key,DES3.MODE_CBC,iv)
-        msg = iv+cipher.encrypt(source)
+        cipher = DES3.new(key,DES3.MODE_CBC,self.getIV(iv))
+        msg = cipher.encrypt(self.appendPadding(DES3.block_size,source))
         msg=base64.encodestring(msg)
         return msg
-    def decode(self,key,source):
+    def decode(self,source,key,iv):
         key =self.getKey(key)
         source=base64.decodestring(source)
-        iv = Random.new().read(DES3.block_size)
-        cipher = DES3.new(key, DES3.MODE_CBC,iv)
+        cipher = DES3.new(key, DES3.MODE_CBC,self.getIV(iv))
         msg =cipher.decrypt(source)
-        return msg[8:len(msg)]
+        return msg
+
+    def  generateSecretKey(self):
+        return Random.new.read(DES3.MODE_CBC)
+
+    def randomIVBytes(self):
+        return Random.new().read(DES3.block_size)
+
+
+
+
 
 if __name__=="__main__":
-    # ysedRi2FrlST+cQsk2DD4DphLQcvzpT6
     obj=DESUtils()
-    rs=obj.encode("www.zcline.net","1103010900000013")
-    print rs
-    print obj.decode("www.zcline.net",rs)
+    key="www.zcline.net"
+    iv="artwebs"
+    text="1103010900000013"
+    rs=obj.encode(text,key,iv)
+    print(rs)
+    print obj.decode(rs,key,iv)
+    print obj.decode("bXgKYTR47dosKznX/32ARzoeuuBsdfIn",key,iv)
+    print obj.decode("bXgKYTR47dosKznX/32ARw==",key,iv)
+
+    print(base64.decodestring("RzoeuuBsdfIn"))
--- awspy/security/Security.py
+++ awspy/security/Security.py
@@ -1,11 +1,47 @@
+# -*- coding:utf-8 -*-
 __author__ = 'artwebs'
+
+import string
+import struct
 
 class Security(object):
      def getKey(self,key):
-        rskey="";
+        rs=[0]*24
+        num=0
         for i in range(24):
-            if i<len(key):
-                rskey+=key[i]
-            else :
-                rskey+="0"
-        return rskey;
+            if num>=len(key):
+                num=0
+            rs[i]=key[num]
+            num+=1
+        return ''.join(rs)
+     def getIV(self,iv):
+         rs=[0]*8
+         num=0
+         for i in range(8):
+             if num>=len(iv):
+                 num=0
+             rs[i]=iv[num]
+             num+=1
+         return ''.join(rs)
+
+     def nrPadBytes(self,blocksize, size):
+        'Return number of required pad bytes for block of size.'
+        if not (0 < blocksize < 255):
+            raise NameError('blocksize must be between 0 and 255')
+        return blocksize - (size % blocksize)
+
+     def appendPadding(self,blocksize, s):
+        '''Append rfc 1423 padding to string.
+
+        RFC 1423 algorithm adds 1 up to blocksize padding bytes to string s. Each
+        padding byte contains the number of padding bytes.
+        '''
+        n = self.nrPadBytes(blocksize, len(s))
+        return s + (chr(n) * n)
+
+     def removePadding(self,blocksize, s):
+        'Remove rfc 1423 padding from string.'
+        n = ord(s[-1]) # last byte contains number of padding bytes
+        if n > blocksize or n > len(s):
+            raise NameError('invalid padding')
+        return s[:-n]
