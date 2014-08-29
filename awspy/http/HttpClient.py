2388e72e74cfb3a6235cbf8ba4c6f87a2f7aa68c
artwebs <artwebs@126.com>
2014-08-29 15:21:24 +0800

    优化httpclient生成

--- awspy/http/HttpClient.py
+++ awspy/http/HttpClient.py
@@ -1,4 +1,4 @@
-# -*- coding:gbk -*- 
+# -*- coding: utf-8 -*-
 '''
 Created on 2010-10-26
 
@@ -70,6 +70,29 @@ class HttpClient(object):
         rsarr=[response.status,response.reason,data]       
         conn.close()
         return rsarr
+
+    def dopostContent(self,url,args=None,timeout=10,charset='utf8'):
+        arrurl=self.unurl(url)
+        if args is None:
+            args=self.getargs(arrurl[4])
+        hostarr=arrurl[1].split(":")
+        host=hostarr[0]
+        if len(hostarr)==2:
+            port=hostarr[1]
+        else:
+            port="80"
+        headers = {"Content-type": "application/x-www-form-urlencoded","Accept": "text/plain"}
+        conn = httplib.HTTPConnection(host,port,timeout)
+        conn.request("POST", arrurl[2], args, headers)
+        response = conn.getresponse()
+        data = response.read()
+        try:
+            data=data.decode(charset)
+        except:
+            pass
+        rsarr=[response.status,response.reason,data]
+        conn.close()
+        return rsarr
     
     def unurl(self,url):        
         return urlparse(url)   
@@ -94,10 +117,10 @@ def test():
 #    u=HttpClient()
 ##    rs=u.doget("http://localhost:8686/LHBSystem/index.php")
 #    m=Method();
-#    a=m.toencode("中国好的德尔");
+#    a=m.toencode("涓浗濂界殑寰峰皵");
 #    
 #    url="http://localhost:8686/LHBSystem/index.php?aaa="+a;
-##    url="http://localhost:8686/LHBSystem/index.php?aaa=中国好的德尔";
+##    url="http://localhost:8686/LHBSystem/index.php?aaa=涓浗濂界殑寰峰皵";
 #    print url
 #    rs=u.dopost(url)
 #    rs=u.doget("http://www.126.com")
